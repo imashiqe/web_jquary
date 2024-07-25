1deb10eb3b3de125267d06ce9801662e162225e3
Ashiqur Rahman <imashiqe@gmail.com>
2024-07-25 23:46:59 +0600

    web  jquary practice 2019

---
+++ 11 java script/01/js/main.js
@@ -0,0 +1,14 @@
+// String("");
+// nun/int=12345678910;
+// Boolean= true/false;
+// undefined=undefined;
+// null=
+// Object =
+// NaN = not a Number; 
+
+// alert();
+// console.log();
+// document.write();
+// innerHTML
+
+alert (hello world);
---
+++ 11 java script/02/js/main.js
@@ -0,0 +1,14 @@
+// String("");
+// num/int=1235;
+// Boolean=true/false;
+// undefined=undefined;
+// null=
+// Object=
+// nan=not a Number;
+
+// alert();
+// console.log();
+// document.write()
+// innerHTML
+
+alert("Hello How Are you What are you doing now can you explain me");
